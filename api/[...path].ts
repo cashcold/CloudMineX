142132a549d7656e1ad491c560412d89ddd5b3db
Frank99 <frankainoo@gmail.com>
2026-09-05 00:23:18 +0000

    fix duplicate Vercel API route

--- api/[...path].ts
+++
@@ -1,7 +0,0 @@
-import type { Request, Response } from 'express';
-import { app, initializeApp } from '../server';
-
-export default async function handler(req: Request, res: Response) {
-  await initializeApp();
-  return app(req, res);
-}
